d3e796aea737d90902172a2c78d2cc2c46fc0d46
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2025-11-17 19:35:44 -0500

    Update to allow it to compile See https://github.com/kapitainsky/RcloneBrowser/issues/207 for more details

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -432,8 +432,9 @@ void MainWindow::rcloneGetVersion() {
           };
 #endif
 
-          QStringList lines = version.split("\n", QString::SkipEmptyParts);
-          QString rclone_info2;
+         // QStringList lines = version.split("\n", QString::SkipEmptyParts);
+         QStringList lines = version.split("\n", Qt::SkipEmptyParts); 
+         QString rclone_info2;
           QString rclone_info3;
 
           int counter = 0;
@@ -1214,7 +1215,10 @@ void MainWindow::addStream(const QString &remote, const QString &stream) {
   ui.jobs->insertWidget(1, line);
   ui.tabs->setTabText(1, QString("Jobs (%1)").arg(++mJobCount));
 
-  player->start(stream, QProcess::ReadOnly);
+  // player->start(stream, QProcess::ReadOnly);
+  player->setProgram(stream);
+  player->start(QProcess::ReadOnly);  
+  
   UseRclonePassword(rclone);
   rclone->start(GetRclone(),
                 QStringList() << "cat" << GetRcloneConf() << remote,
